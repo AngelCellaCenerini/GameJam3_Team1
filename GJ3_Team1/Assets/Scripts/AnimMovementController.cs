7a123aebaaad7bc2880a592a9a6aabfec4487419
Angel CC <angelcenerini@gmail.com>
2022-11-02 00:01:09 -0400

    SET UP
    Rough scene + Character controller

---
+++ GJ3_Team1/Assets/Scripts/AnimMovementController.cs
@@ -0,0 +1,205 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimMovementController : MonoBehaviour
+{
+    // Reference
+    PlayerInput playerInput;
+    CharacterController characterController;
+    Animator animator;
+
+    int isWalkingHash;
+    int isRunningHash;
+
+    // Movement
+    Vector2 currentMovementInput;
+    Vector3 currentMovement;
+    Vector3 currentRunMovement;
+    // States
+    bool isMovementPressed;
+    bool isRunPressed;
+    bool isJumpPressed = false;
+    bool isJumping = false;
+    // Multipliers
+    float runMultiplier = 3.0f;
+    float rotationFractorPerFrame = 1.0f;
+    float initialJumpVelocity;
+    float maxJumpHeight = 1.0f;
+    float maxJumpTime = 0.5f;
+    // Gravity
+    float groundedGravity = -.05f;
+    float gravity = -9.18f;
+
+
+    void Awake()
+    {
+        // Reference
+        playerInput = new PlayerInput();
+        characterController = GetComponent<CharacterController>();
+        // animator = GetComponent<Animator>();
+
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+
+        // Register Input & Cancel Movement
+        // Walk
+        playerInput.CharacterControls.Move.performed += onMovementInput;
+        playerInput.CharacterControls.Move.canceled += onMovementInput;
+        // Run
+        playerInput.CharacterControls.Run.performed += onRun;
+        playerInput.CharacterControls.Run.canceled += onRun;
+        // Jump
+        playerInput.CharacterControls.Jump.started += onJump;
+        playerInput.CharacterControls.Jump.canceled += onJump;
+
+        setJumpVariables();
+
+    }
+
+    void onMovementInput(InputAction.CallbackContext context)
+    {
+        // Manage Call back functions
+        // Walk
+        currentMovementInput = context.ReadValue<Vector2>();
+        currentMovement.x = currentMovementInput.x;
+        currentMovement.z = currentMovementInput.y;
+        // Run
+        currentRunMovement.x = currentMovementInput.x * runMultiplier;
+        currentRunMovement.z = currentMovementInput.y * runMultiplier;
+        // Check if moving
+        isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
+    }
+
+    void onRun(InputAction.CallbackContext context)
+    {
+        isRunPressed = context.ReadValueAsButton();
+    }
+
+    void onJump(InputAction.CallbackContext context)
+    {
+        isJumpPressed = context.ReadValueAsButton();
+    }
+
+    void setJumpVariables()
+    {
+        // Calculate Gravity
+        float timeToApex = maxJumpTime / 2;
+        gravity = (-2 * maxJumpHeight) / Mathf.Pow(timeToApex, 2);
+        initialJumpVelocity = (2 * maxJumpHeight) / timeToApex;
+    }
+
+    void handleJump()
+    {
+        // Check if jumping
+        if (!isJumping && characterController.isGrounded && isJumpPressed)
+        {
+            // Jump
+            isJumping = true;
+            currentMovement.y = initialJumpVelocity;
+            currentRunMovement.y = initialJumpVelocity;
+        }
+        else if(!isJumpPressed && isJumping && characterController.isGrounded)
+        {
+            // Restore State
+            isJumping = false;
+        }
+    }
+
+    void handleRotation()
+    {
+        // Handle Animation Direction on Rotation
+        Vector3 positionToLookAt;
+        positionToLookAt.x = currentMovement.x;
+        positionToLookAt.y = 0.0f;
+        positionToLookAt.z = currentMovement.y;
+
+        // Register current rotation
+        Quaternion currentRotation = transform.rotation;
+        // Create new rotation
+        if (isMovementPressed)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(positionToLookAt);
+            transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationFractorPerFrame * Time.deltaTime);
+        }
+    }
+
+    void handleAnimation() {
+        // Manage Animation Switches
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool isRunning = animator.GetBool(isRunningHash);
+
+        // Animate
+        if (isMovementPressed && !isWalking)
+        {
+            // Walk
+            animator.SetBool("isWalking", true);
+        }
+        else if (!isMovementPressed && isWalking)
+        {
+            // Stop Walking
+            animator.SetBool("isWalking", false);
+        }
+
+        if ((isMovementPressed && isRunPressed) && !isRunning)
+        {
+            // Run
+            animator.SetBool("isRunning", true);
+        }
+        else if ((!isMovementPressed || isRunPressed) && isRunning)
+        {
+            // Stop Running
+            animator.SetBool("isRunning", false);
+        }
+    }
+
+    void handleGravity()
+    {
+        if (characterController.isGrounded)
+        {
+            // Apply Downwards Force (Gravity)
+            currentMovement.y = groundedGravity;
+            currentRunMovement.y = groundedGravity;
+        }
+        else
+        {
+            currentMovement.y += gravity * Time.deltaTime;
+            currentRunMovement.y += gravity * Time.deltaTime;
+        }
+    }
+
+
+    void Update()
+    {
+        // Move
+        // Check if running
+        if (isRunPressed)
+        {
+            characterController.Move(currentRunMovement * Time.deltaTime);
+        }
+        else
+        {
+            characterController.Move(currentMovement * Time.deltaTime);
+        }
+
+        // Animate
+        // handleAnimation();
+        // Update Direction
+        // handleRotation();
+
+        handleGravity();
+        handleJump();
+    }
+
+    void OnEnable()
+    {
+        // Enable Input System (Action Map)
+        playerInput.CharacterControls.Enable();
+    }
+    void OnDisable()
+    {
+        // Disable Input System (Action Map)
+        playerInput.CharacterControls.Disable();
+    }
+}
