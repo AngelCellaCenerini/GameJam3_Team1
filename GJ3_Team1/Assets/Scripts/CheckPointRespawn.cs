ef3a2e0be8a68d74a5e6985e05c575e2d157a81e
Angel CC <angelcenerini@gmail.com>
2022-11-07 22:16:12 -0500

    CheckPoint Mechanic
    First floor to be decided

---
+++ GJ3_Team1/Assets/Scripts/CheckPointRespawn.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointRespawn : MonoBehaviour
+{
+    Transform respawnCoordinates;
+    public Transform respawnCoordinates1;
+    public Transform respawnCoordinates2;
+    public GameObject player;
+
+    bool progress1 = false;
+    bool progress2 = false;
+    bool currentProgress;
+
+    void Start()
+    {
+        respawnCoordinates = respawnCoordinates1;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if Player meets Checkpoint
+        // Identify Checkpoint
+        if (other.gameObject.name == "CheckPoinTrigger1")
+        {
+            progress1 = true;
+            currentProgress = progress1;
+            respawnCoordinates = respawnCoordinates1;
+        }
+        else if (other.gameObject.name == "CheckPoinTrigger2")
+        {
+            progress2 = true;
+            currentProgress = progress2;
+            respawnCoordinates = respawnCoordinates2;
+        }
+
+        // Check if Player drops off platform
+        if (other.gameObject.tag == "DropOff")
+        {
+            // Check if User Reached CHeckpoint
+            if (currentProgress)
+            {
+                // Respawn Player
+                player.transform.position = respawnCoordinates.transform.position;
+            }           
+        }
+    }
+}
